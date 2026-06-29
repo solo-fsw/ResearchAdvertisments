d9b95f81313158ffff942ba3e4ccfb1754ac8d6c
LabsupportEvert <98744988+LabsupportEvert@users.noreply.github.com>
2026-06-29 12:09:10 +0200

    commit

---
+++ slides/script.js
@@ -0,0 +1,120 @@
+const slides = [
+
+"images/Slide1.jpg",
+"images/Slide2.jpg",
+"images/Slide3.jpg"
+
+];
+
+
+const gallery=document.getElementById("gallery");
+
+
+// thumbnails maken
+
+slides.forEach((slide,index)=>{
+
+
+let img=document.createElement("img");
+
+img.src=slide;
+
+img.className="thumb";
+
+
+img.onclick=function(){
+
+openSlide(index);
+
+};
+
+
+gallery.appendChild(img);
+
+
+});
+
+
+
+let current=0;
+
+
+const lightbox=document.getElementById("lightbox");
+const bigImage=document.getElementById("bigImage");
+const counter=document.getElementById("counter");
+
+
+
+function openSlide(index){
+
+current=index;
+
+update();
+
+lightbox.style.display="flex";
+
+}
+
+
+
+function update(){
+
+bigImage.src=slides[current];
+
+counter.innerHTML=
+(current+1)+" / "+slides.length;
+
+}
+
+
+
+document.getElementById("next").onclick=function(){
+
+current++;
+
+if(current>=slides.length)
+current=0;
+
+update();
+
+}
+
+
+
+document.getElementById("prev").onclick=function(){
+
+current--;
+
+if(current<0)
+current=slides.length-1;
+
+update();
+
+}
+
+
+
+document.getElementById("close").onclick=function(){
+
+lightbox.style.display="none";
+
+}
+
+
+
+document.addEventListener("keydown",function(e){
+
+
+if(e.key==="ArrowRight")
+document.getElementById("next").click();
+
+
+if(e.key==="ArrowLeft")
+document.getElementById("prev").click();
+
+
+if(e.key==="Escape")
+lightbox.style.display="none";
+
+
+});
